56296c078b72b04ea150275e6fc5d68d7bbf1f61
SzekeresBoroka <szekeresboroka@gmail.com>
2020-02-29 14:29:19 +0200

    forward checking

---
+++ Source.cpp
@@ -0,0 +1,208 @@
+#include<stdio.h> 
+#include<stdbool.h> 
+
+// csucsok szama
+#define V 7
+//szinek szama
+#define M 3
+
+int ertekadasok = 0;
+
+void printSolution(int color[]);
+
+//backtracking: v csucs kaphatja-e a c szint?
+bool isSafe(int v, bool graph[V][V], int color[], int c)
+{
+	for (int i = 0; i < V; i++)
+		if (graph[v][i] && c == color[i])
+			return false;
+	return true;
+}
+
+//backtracking
+bool graphColoring_backtracking(bool graph[V][V], int color[], int v)
+{
+	//minden csucsnak van szine
+	if (v == V)
+		return true;
+
+	//v csucsnak sorra adjuk a szineket
+	for (int c = 1; c <= M; c++)
+	{
+		ertekadasok++;
+		//a c szin megfelel-e
+		if (isSafe(v, graph, color, c))
+		{
+			color[v] = c;
+
+			//kovetkezo csucs
+			if (graphColoring_backtracking(graph, color, v + 1) == true)
+				return true;
+
+			//a valasztott szin vezet megoldashoz, viszalep
+			color[v] = 0;
+		}
+	}
+
+	//nincs megfelelo szin
+	return false;
+}
+
+//forward checking: visszalepeskor visszakerulnek a kitorolt szinek
+void putBackColor(int t[], int c) {
+	for (int i = 0; i < M; i++) {
+		if (t[i] == 0) {
+			t[i] = c;
+			return;
+		}
+	}
+}
+
+void print(int t[], int n) {
+	for (int i = 0; i < n; i++) {
+		printf("%d ", t[i]);
+	}
+	printf("\n");
+}
+
+void print2d(int t[V][M]) {
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ", t[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// forward checking: toroljuk a szomszedok lehetseges szinei kozul, amit a v-nek valasztottunk
+bool deleteInconsistentValues(bool graph[V][V], int v, int c, int forward_check_list[V][M]) {
+	for (int i = 0; i < V; i++) {
+		if (graph[v][i]) {
+			for (int j = 0; j < M; j++) {
+				if (c == forward_check_list[i][j]) {
+					forward_check_list[i][j] = 0;
+					bool kilep = true;
+					for (int k = 0; k < M; k++) {
+						if (forward_check_list[i][k] != 0) {
+							kilep = false;
+							break;
+						}
+					}
+					if (kilep) {
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
+
+//forward checking: visszadja azt a szint amit meg nem toroltunk ki a csucs lehetseges szinei kozul
+int chooseColor(int t[]) {
+	for (int i = 0; i < M; i++) {
+		if (t[i] != 0)
+			return t[i];
+	}
+}
+
+// backtracking + forward checking
+bool graphColoring_forwardChecking(bool graph[V][V], int color[], int v, int forward_check_list[V][M])
+{
+	//minden csucsnak van szine
+	if (v == V)
+		return true;
+
+	int c = chooseColor(forward_check_list[v]); // valasztott szin
+	color[v] = c;
+	ertekadasok++;
+	if (deleteInconsistentValues(graph, v, c, forward_check_list) == true) {
+		if (graphColoring_forwardChecking(graph, color, v + 1, forward_check_list) == true) {
+			return true;
+		}
+		color[v] = 0;
+		for (int i = 0; i < V; i++) {
+			if (graph[v][i]) { // ha szomszedos
+				putBackColor(forward_check_list[i], c); //visszatesszuk a szomszedok lehetseges szinei koze
+			}
+		}
+	}
+
+	//nincs megfelelo szin
+	return false;
+}
+
+//terkepszinezes problema megoldasa 3 algoritmussal
+void graphColoring(bool graph[V][V])
+{
+	//szin tomb inicialaizalasa
+	int color[V];
+	for (int i = 0; i < V; i++)
+		color[i] = 0;
+
+	// nyers backtracking
+	if (graphColoring_backtracking(graph, color, 0) == false)
+	{
+		printf("nyers backtracking: nincs megoldas\n");
+	}
+	else {
+		printf("nyers backtracking:\n");
+		printSolution(color);
+	}
+
+	//a forward_check_list -ben taroljuk a csucsok lehetseges szineit, 
+	//inicializaljuk mindenik csucsot mindenik szinnel
+	int forward_check_list[V][3];
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j < 3; j++) {
+			forward_check_list[i][j] = j + 1;
+		}
+	}
+
+	//szin tomb inicializalasa
+	for (int i = 0; i < V; i++)
+		color[i] = 0;
+
+	//backtracking + forward checking
+	if (graphColoring_forwardChecking(graph, color, 0, forward_check_list) == false) {
+		printf("backtracking + forward checking: nincs megoldas\n");
+	}
+	else {
+		// Print the solution
+		printf("backtracking + forward checking:\n");
+		printSolution(color);;
+	}
+}
+
+void printSolution(int color[])
+{
+	for (int i = 0; i < V; i++)
+		printf(" %d ", i+1);
+	printf("\t -> csucsok\n");
+	for (int i = 0; i < V; i++)
+		printf(" %d ", color[i]);
+	printf("\t -> szinek\n");
+	printf("ertekadasok szama: %d\n\n", ertekadasok);
+	ertekadasok = 0;
+}
+
+
+int main()
+{
+	bool graph[V][V] = {
+		{0, 1, 1, 0, 0, 0, 0},
+		{1, 0, 1, 1, 0, 0, 0},
+		{1, 1, 0, 1, 1, 1, 0},
+		{0, 1, 1, 0, 1, 0, 0},
+		{0, 0, 1, 1, 0, 1, 0},
+		{0, 0, 1, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}
+	};
+	
+	graphColoring(graph);
+
+	//varjon entert
+	char c;
+	c = getchar();
+	return 0;
+}
